9b30fe0189a7962e6ea476b99ec92eefcdd5e360
Ken Domino <kenneth.domino@domemtech.com>
2021-04-06 07:40:28 -0400

    no message

---
+++ r/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace r
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            for (; ; )
+            {
+                var s = System.Console.ReadLine();
+                if (s == null)
+                {
+                    break;
+                }
+                lines.Add(s);
+            }
+            System.Console.WriteLine("Got " + String.Join(" ", lines));
+        }
+    }
+}
---
+++ w/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace w
+{
+	public class Config
+	{
+		[Option("version", Required = false)]
+		public bool? Version { get; set; }
+
+		[Value(1)]
+		public IEnumerable<int> Times { get; set; }
+
+		[Option("type", Required = false)]
+		public string? Type { get; set; }
+	}
+
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int count = 1;
+			var result = Parser.Default.ParseArguments<Config>(args)
+			.WithParsed(options =>
+			{
+				if (options.Times != null)
+				{
+					var i = options.Times.GetEnumerator();
+					if (i.MoveNext()) count = i.Current;
+				}
+			}
+			)
+			.WithNotParsed(errors =>
+			{
+			});
+
+			System.Console.Error.WriteLine("Parse completed of C:\\Users\\kenne\\Documents\\AntlrVSIX\\test\\A.g4");
+			for (int j = 0; j < count; ++j)
+			{
+				var s = @"{
+  ""FileName"": ""C:\\Users\\kenne\\Documents\\AntlrVSIX\\test\\A.g4""
+}";
+				System.Console.Write(s);
+			}
+		}
+    }
+}
